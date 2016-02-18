049069d727d06af7d82a09b074810b22fb841633
Reinaldo <reinaldo.java@gmail.com>
2016-02-18 18:10:35 -0200

    Criação do Projeto

---
+++ Ranking/src/br/com/teste/TesteRanking.java
@@ -0,0 +1,18 @@
+package br.com.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteRanking {
+
+	@Test
+	public void testeSoma() throws Exception {
+		int valorA = 1;
+		int valorB = 2;
+		int soma = 0;
+
+		assertEquals(3, soma);
+	}
+
+}
